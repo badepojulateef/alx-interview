6065a95e8278723a622a00556d1e8752ee4dd10d
Adepoju Lateef <badepojulateef@gmail.com>
2023-11-24 00:36:27 +0100

    updated project task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -30,9 +30,10 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    [
+        dp.__setitem__(i, min(dp[i], dp[i - coin] + 1))
+        for coin in coins
+        for i in range(coin, total + 1)
+    ]
 
     return dp[total] if dp[total] != float('inf') else -1
